ae4891dc4bfe19903201e94b93b8b24e288e7fa1
s-makino <sin.makino@gmail.com>
2020-01-22 16:53:52 +0900

    Add BLEDevice.manufacturerData

--- src/BLEDevice.h
+++ src/BLEDevice.h
@@ -50,12 +50,14 @@ public:
   virtual String address() const;
 
   bool hasLocalName() const;
-    
+  bool hasManufacturerData() const;
+
   bool hasAdvertisedServiceUuid() const;
   bool hasAdvertisedServiceUuid(int index) const;
   int advertisedServiceUuidCount() const;
 
   String localName() const;
+  String manufacturerData() const;
   String advertisedServiceUuid() const;
   String advertisedServiceUuid(int index) const;
 
