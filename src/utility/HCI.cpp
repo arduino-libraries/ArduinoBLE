039df0a51d51d79b42461f916bc36a50723f1ac8
Oliver <otlg123@gmail.com>
2020-11-14 14:05:47 +0000

    Allocate dyamic sized arrays

--- src/utility/HCI.cpp
+++ src/utility/HCI.cpp
@@ -630,7 +630,7 @@ int HCIClass::sendAclPkt(uint16_t handle, uint8_t cid, uint8_t plen, void* data)
     uint16_t cid;
   } aclHdr = { HCI_ACLDATA_PKT, handle, uint8_t(plen + 4), plen, cid };
 
-  uint8_t txBuffer[sizeof(aclHdr) + plen];
+  uint8_t *txBuffer = uint8_t*malloc(sizeof(aclHdr) + plen);
   memcpy(txBuffer, &aclHdr, sizeof(aclHdr));
   memcpy(&txBuffer[sizeof(aclHdr)], data, plen);
 
@@ -648,7 +648,8 @@ int HCIClass::sendAclPkt(uint16_t handle, uint8_t cid, uint8_t plen, void* data)
 
   _pendingPkt++;
   HCITransport.write(txBuffer, sizeof(aclHdr) + plen);
-
+  free(txBuffer);
+  
   return 0;
 }
 
@@ -680,7 +681,7 @@ int HCIClass::sendCommand(uint16_t opcode, uint8_t plen, void* parameters)
     uint8_t plen;
   } pktHdr = {HCI_COMMAND_PKT, opcode, plen};
 
-  uint8_t txBuffer[sizeof(pktHdr) + plen];
+  uint8_t *txBuffer = uint8_t*malloc(sizeof(pktHdr) + plen);
   memcpy(txBuffer, &pktHdr, sizeof(pktHdr));
   memcpy(&txBuffer[sizeof(pktHdr)], parameters, plen);
 
@@ -704,6 +705,8 @@ int HCIClass::sendCommand(uint16_t opcode, uint8_t plen, void* parameters)
   for (unsigned long start = millis(); _cmdCompleteOpcode != opcode && millis() < (start + 1000);) {
     poll();
   }
+
+  free(txBuffer);
 
   return _cmdCompleteStatus;
 }
