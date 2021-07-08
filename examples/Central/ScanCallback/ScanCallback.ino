faffc11b61b3b56e91914c0f9accba11c7fcac97
Frederic Pillon <frederic.pillon@st.com>
2021-07-08 14:33:34 +0200

    examples: add STM32WB5MM-DK support
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- examples/Central/ScanCallback/ScanCallback.ino
+++ examples/Central/ScanCallback/ScanCallback.ino
@@ -30,8 +30,7 @@ HCISpiTransportClass HCISpiTransport(SpiHCI, SPBTLE_RF, PD13, PE6, PA8, 8000000,
 BLELocalDevice BLEObj(&HCISpiTransport);
 BLELocalDevice& BLE = BLEObj;
 #endif
-#elif defined(ARDUINO_P_NUCLEO_WB55RG)
-/* PNUCLEO_WB55RG */
+#elif defined(ARDUINO_P_NUCLEO_WB55RG) || defined(ARDUINO_STM32WB5MM_DK)
 HCISharedMemTransportClass HCISharedMemTransport;
 #if !defined(FAKE_BLELOCALDEVICE)
 BLELocalDevice BLEObj(&HCISharedMemTransport);
