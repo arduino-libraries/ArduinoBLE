751295300e5fb7fc1f277410e8aa76f4e29f7302
Frederic Pillon <frederic.pillon@st.com>
2022-12-12 17:17:48 +0100

    fix: include a timeout when waiting for the cmd_resp
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- src/utility/STM32Cube_FW/shci_tl.c
+++ src/utility/STM32Cube_FW/shci_tl.c
@@ -23,6 +23,7 @@
 #include "stm_list.h"
 #include "shci_tl.h"
 #include "stm32_def.h"
+#include "wiring_time.h"
 
 /* Private typedef -----------------------------------------------------------*/
 typedef enum
@@ -250,11 +251,12 @@ static void TlUserEvtReceived(TL_EvtPacket_t *shcievt)
 /* Weak implementation ----------------------------------------------------------------*/
 __WEAK void shci_cmd_resp_wait(uint32_t timeout)
 {
-  (void)timeout;
-
   CmdRspStatusFlag = SHCI_TL_CMD_RESP_WAIT;
-  while(CmdRspStatusFlag != SHCI_TL_CMD_RESP_RELEASE);
-
+  for (unsigned long start = millis(); (millis() - start) < timeout;) {
+    if (CmdRspStatusFlag == SHCI_TL_CMD_RESP_RELEASE) {
+      break;
+    }
+  }
   return;
 }
 
