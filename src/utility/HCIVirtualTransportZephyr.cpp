481f6acef1e29fb37f27d48789a7336202d884cb
iabdalkader <i.abdalkader@gmail.com>
2025-08-06 13:46:48 +0200

    Add firmware loading support for CYW43.
    Signed-off-by: iabdalkader <i.abdalkader@gmail.com>

--- src/utility/HCIVirtualTransportZephyr.cpp
+++ src/utility/HCIVirtualTransportZephyr.cpp
@@ -20,14 +20,104 @@
 #if defined(__ZEPHYR__)
 
 #include "HCIVirtualTransportZephyr.h"
+#include "HCI.h"
 
 #include <zephyr/bluetooth/buf.h>
 #include <zephyr/bluetooth/hci.h>
 #include <zephyr/bluetooth/hci_raw.h>
+#include <zephyr/drivers/uart.h>
 
 static K_FIFO_DEFINE(rx_queue);
 struct k_fifo* __rx_queue = &rx_queue;
 extern "C" int bt_h4_vnd_setup(const struct device *dev);
+
+#if CONFIG_CYW4343W_MURATA_1DX
+// External firmware defines.
+extern const uint8_t brcm_patchram_buf[];
+extern const int brcm_patch_ram_length;
+
+enum {
+  HCI_VND_OP_FW_DOWNLOAD  = 0xFC2E,
+  HCI_VND_OP_WRITE_RAM    = 0xFC4C,
+  HCI_VND_OP_LAUNCH_RAM   = 0xFC4E,
+  HCI_VND_OP_SET_BAUDRATE = 0xFC18,
+};
+
+static int cyw4343_set_baudrate(const struct device *uart, uint32_t baudrate) {
+    struct __attribute__ ((packed)) {
+      uint16_t zero;
+      uint32_t baud;
+    } param = { 0, baudrate };
+
+	struct uart_config uart_cfg;
+	if (uart_config_get(uart, &uart_cfg)) {
+		return -1;
+	}
+
+    if (HCI.sendCommand(HCI_VND_OP_SET_BAUDRATE, sizeof(param), (void *) &param)) {
+      return -1;
+    }
+
+    uart_cfg.baudrate = baudrate;
+    if (uart_configure(uart, &uart_cfg)) {
+        return -1;
+    }
+
+    uart_irq_rx_enable(uart);
+    return 0;
+}
+
+static int cyw4343_download_firmware(const struct device *uart) {
+  #define MURATA_NODE DT_CHILD(DT_CHOSEN(zephyr_bt_hci), murata_1dx)
+  uint32_t operational_speed = DT_PROP_OR(MURATA_NODE, hci_operation_speed, 115200);
+  uint32_t fw_download_speed = DT_PROP_OR(MURATA_NODE, fw_download_speed, 115200);
+
+  // Reset controller
+  if (HCI.sendCommand(0x0C03, 0, NULL)) {
+    return -1;
+  }
+
+  // Switch to fast baudrate
+  if ((operational_speed != fw_download_speed) && 
+      cyw4343_set_baudrate(uart, fw_download_speed)) {
+      return -1;
+  }
+
+  // Start firmware downloader.
+  if (HCI.sendCommand(HCI_VND_OP_FW_DOWNLOAD, 0, NULL)) {
+    return -1;
+  }
+
+  // Load the firmware image.
+  for (size_t offset=0; offset < brcm_patch_ram_length;) {
+    uint8_t  length = brcm_patchram_buf[offset + 2];
+    uint16_t opcode = (brcm_patchram_buf[offset + 0]) |
+                      (brcm_patchram_buf[offset + 1] << 8);
+
+    // Opcode should be write RAM or launch RAM.
+    if (opcode != HCI_VND_OP_WRITE_RAM && opcode != HCI_VND_OP_LAUNCH_RAM) {
+      return -1;
+    }
+
+    if (HCI.sendCommand(opcode, length, (void *) &brcm_patchram_buf[offset + 3])) {
+      return -1;
+    }
+
+    offset += length + 3;
+  }
+
+  // Delay after firmware loading.
+  delay(250);
+
+  // Switch back to the default baudrate.
+  if ((operational_speed != fw_download_speed) && 
+      cyw4343_set_baudrate(uart, operational_speed)) {
+      return -1;
+  }
+
+  return 0;
+}
+#endif
 
 HCIVirtualTransportZephyrClass::HCIVirtualTransportZephyrClass() {
 
@@ -49,6 +139,12 @@ int HCIVirtualTransportZephyrClass::begin() {
   if (bt_h4_vnd_setup(uart)) {
     return 0;
   }
+
+#if CONFIG_CYW4343W_MURATA_1DX
+  if (cyw4343_download_firmware(uart)) {
+    return 0;
+  }
+#endif /* CONFIG_CYW4343W_MURATA_1DX */
 #endif /* CONFIG_BT_HCI_SETUP */
 
   rxbuf.clear();
