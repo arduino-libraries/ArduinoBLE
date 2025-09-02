5915fa2df776e6eb7e9e5afd896ed3d1c048c9c0
fabik111 <fabiomassimo.centonze@gmail.com>
2025-09-02 15:41:20 +0200

    move ble_drv for nina boards to HCINinaSpiTransport

--- src/utility/HCINinaSpiTransport.h
+++ src/utility/HCINinaSpiTransport.h
@@ -13,6 +13,17 @@
 
 #include "HCITransport.h"
 
+class BleDrv
+{
+public:
+    static int bleBegin();
+    static void bleEnd();
+    static int bleAvailable();
+    static int bleRead(uint8_t data[], size_t length);
+    static int blePeek(uint8_t data[], size_t length);
+    static size_t bleWrite(const uint8_t* data, size_t length);
+};
+
 class HCINinaSpiTransportClass : public HCITransportInterface {
 public:
   HCINinaSpiTransportClass() {}
